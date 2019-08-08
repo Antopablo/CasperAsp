46813bc60b04aea3f8a799bdbe7d7823800a13da
Antony LEFEVRE <lefevreanto57@gmail.com>
2019-08-08 15:48:27 +0200

    Ajout style + possibilité d'ajouter un user dans la bdd

--- CasperAsp/Controllers/AdminController.cs
+++ CasperAsp/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CasperAsp.Models;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -17,6 +19,24 @@ namespace CasperAsp.Controllers
         public ActionResult Users()
         {
             return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        public ActionResult Add_validation(User u)
+        {
+            // connection mySQL
+            MySqlConnection myServerSQL = new MySqlConnection("server=137.74.118.171;userid=sta41;persistsecurityinfo=True;password=w5fc03;database=sta41");
+            myServerSQL.Open();
+            MySqlCommand cmd = new MySqlCommand("INSERT INTO Users (Identifiant, Password) VALUES ('"+u.Name +"','"+u.Password+"')", myServerSQL);
+
+            cmd.ExecuteNonQuery();
+            myServerSQL.Close();
+
+            return RedirectToAction("Users");
         }
     }
 }
